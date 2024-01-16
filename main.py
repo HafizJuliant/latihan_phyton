82b76c9da1f4c6cb55d0852187399425d1573b79
Hafiz Juliant <hafizjuliant@gmail.com>
2024-01-16 16:25:43 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,70 @@
+# print(1234411)
+# print(123ad31) # ini error
+# print('123ad31')
+
+# print(type(1234411))
+# print(type('123ad31'))
+
+# print(100 + 5)
+
+# print(3.5)
+# print(type(3.5))
+
+# print(.7)
+# print(3.)
+# print(3.14e-10)
+# print(3.14e-1)
+
+# print('Hello my name is Sapta')
+# print("Hello my name is Sapta")
+# print("sorry, that's my item")
+# print('sorry, that\'s my item')
+
+# True False
+
+# print(type(True))
+# print(10 < 20) # True
+# print(10 <= 20) # True
+# print(10 > 20) # False
+# print(10 >= 20) # False
+# print(10 == 20) # False
+# print(not 10 == 20) # True
+
+# di python ada yang namanya variabel, 
+# dan berfungsi untuk menyimpan value
+
+# num1 = 10
+# num2 = 30
+
+# print(num1 + num2)
+
+# 1num = 20 # error karena variable tidak boleh diawali dengan angka
+# num =  20 # diperbolehkan, karena nama variable boleh alphabet saja
+# num1 = 20 # alpanumeric di ijinkan jika, diawali dengan alphabet
+# _num = 20 # nama variable hanya memperbolehkan mengandung symbol '_'
+
+# num1 = num2 = num3 = 0
+# print(num1)
+# print(num2)
+# print(num3)
+
+# num1=5
+# num2=3
+
+# print(num1 + num2)
+# print(num1 - num2)
+# print(num1 * num2)
+# print(num1 / num2)
+# print(num1 ** num2)
+# print(num1 % num2)
+
+name = 'eRsAPta aRIsTo'
+
+print(name.capitalize())
+print(name.lower())
+print(name.upper())
+print(name.title())
+print(name.swapcase())
+
+# wakeupWord = 'bangun'
+# print(wakeupWord)
